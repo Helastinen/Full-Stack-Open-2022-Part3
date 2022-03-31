9e35ae7ec6e9bcfa78909e31b718824d50700456
Antti <hellsten.antti@gmail.com>
2022-03-31 16:37:18 +0300

    add exercise phonebook 3.6

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -59,13 +58,28 @@ app.get("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
   const body = request.body
   const randomId = Math.floor(Math.random() * 10000)
+  const nameAlreadyExists = persons.some(person => 
+    person.name === body.name)
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name is missing"
+    })
+  } else if (!body.number) {
+    return response.status(400).json({
+      error: "number is missing"
+    })
+  } else if (nameAlreadyExists) {
+    return response.status(400).json({
+      error: "name must be unique"
+    })
+  }
 
   const person = {
     name: body.name,
     number: body.number,
     id: randomId,
   }
-  console.log(person);
 
   persons = persons.concat(person)
 
