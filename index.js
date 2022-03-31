7ca60869e7fcc108c76342c6e26dd3372bb6c196
Antti <hellsten.antti@gmail.com>
2022-03-31 15:30:39 +0300

    add exercise phonebook 3.3

--- index.js
+++ index.js
@@ -35,11 +35,22 @@ app.get("/api/persons", (request, response) => {
 
 app.get("/info", (request, response) => {
   const date = new Date()
-  
+
   response.send(
     `Phonebook has info for ${persons.length} people.<br/>
     ${date}`
   )
+})
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
