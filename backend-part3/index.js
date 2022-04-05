f613b31d39d443ef68037847eb4305b3b50edaee
Antti <hellsten.antti@gmail.com>
2022-04-05 15:26:06 +0300

    add phonebook MongoDB exer 3.15

--- backend-part3/index.js
+++ backend-part3/index.js
@@ -130,10 +130,11 @@ app.put("/api/persons/:id", (request, response) => {
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
 })
 
 // * Middleware if not route is called //
